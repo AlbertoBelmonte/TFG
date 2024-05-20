55761315ab4c509eb20fc1ad751df31f6fc82d22
Alberto Belmonte Gómez <Albertobelmontegomez01@gmail.com>
2024-05-20 18:51:43 +0200

    correción script

--- functions/conf_pc/options/change_pcname/main.sh
+++ functions/conf_pc/options/change_pcname/main.sh
@@ -4,10 +4,7 @@
 export LANG="es_ES.UTF-8"
 
 title="Administración del servidor $(hostname)"
-
 message="\nActualmente su equipo se llama '$(hostname)' ¿Desea cambiar el nombre del equipo?"
-
-dialog --stdout --title "$title" --yesno "$message" 0 0
 
 function input_pcname {
 
@@ -16,26 +13,32 @@ function input_pcname {
 
 }
 
+dialog --stdout --title "$title" --yesno "$message" 0 0
+
 if [ $? -eq 0 ]; then
 
   while true; do
 
     input_pcname
 
-    dialog --stdout --title "$title" --yesno "\nHa puesto '$pcname', ¿Está seguro de que el equipo se llame así?" 0 0
-
     if [ $? -eq 0 ]; then
 
-      echo $pcname > /etc/hostname && ./functions/power_pc/main.sh
-      break
-
-    else
-
-      dialog --stdout --title "$title" --yesno "\n¿Está seguro que quiere salit?" 0 0
+      dialog --stdout --title "$title" --yesno "\nHa puesto '$pcname', ¿Está seguro de que el equipo se llame así?" 0 0
 
       if [ $? -eq 0 ]; then
 
-        input_pcname
+        echo $pcname > /etc/hostname && ./functions/power_pc/main.sh
+        break
+
+      fi
+
+    else
+
+      dialog --stdout --title "$title" --yesno "\n¿Desea salir al menu anterior?" 0 0
+
+      if [ $? -eq 0 ]; then
+
+        exit
 
       fi
 
